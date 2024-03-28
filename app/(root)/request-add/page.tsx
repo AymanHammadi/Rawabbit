4b08619e13a4f5487dc5dc7f44eb1f8d9aea8e0d
Ayman <86633489+AymanHammadi@users.noreply.github.com>
2024-03-29 00:50:48 +0300

    fix

--- app/(root)/request-add/page.tsx
+++ app/(root)/request-add/page.tsx
@@ -1,15 +1,7 @@
 import Request from "@/components/forms/Request";
-// import { getUserById } from "@/lib/actions/user.action";
-// import { auth } from "@clerk/nextjs";
-import { redirect } from "next/navigation";
 import React from "react";
 
 const Page = async () => {
-  // const { userId } = auth();
-
-  // if (!userId) redirect("/sign-in");
-
-  // const mongoUser = await getUserById({ userId });
 
   return (
     <div>
